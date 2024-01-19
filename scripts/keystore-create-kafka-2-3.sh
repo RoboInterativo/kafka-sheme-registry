8d8f2996d2c70cce09b9ac919cc7fee07b275c8f
chiefexb <chief_exb@mail.ru>
2024-01-19 11:03:00 +0300

    123

--- scripts/keystore-create-kafka-2-3.sh
+++ scripts/keystore-create-kafka-2-3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /root/kafka-sheme-registry
+
 # Create server key & certificate signing request(.csr file)
 # openssl genrsa -out ca.key
 # openssl req -x509 -new -nodes -key ca.key -sha256 -days 1024 -out ca.crt   -extfile ca.cnf
